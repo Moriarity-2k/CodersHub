3d521a83557fadfe913c01e0f63b05cc7e2d419f
Moriarity-2k <professormoriarity2000@gmail.com>
2023-11-19 07:00:09 +0530

    Before Deploy 4 Minor BUG

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -9,13 +9,13 @@ const AskQueston = async () => {
 
   if (!userId) redirect("/sign-in");
 
-  const mongoUser = await getUserByID(userId);
+  const mongoUser = await getUserByID({ userId });
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
     </div>
   );
